13ded6a6f91b3cf5961b0ad3dc5a373be16ee01c
Rodrigo Ramos Iamim <iamim.rodrigo.42@gmail.com>
2025-08-03 19:47:45 -0300

    update 3ago25

--- Primeiros passos/Lógica com JS/logica-js-projeto_inicial/app.js
+++ Primeiros passos/Lógica com JS/logica-js-projeto_inicial/app.js
@@ -1,21 +1,33 @@
 alert('Bem vindo, ao jogo do número secreto! Será que você consegue adivinhar o número correto?');
-let numeroSecreto = 7;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute
 let tentativas = 1
 
-//enquanto não atender ao requisito chute == numeroSecreto
+// enquanto não atender ao requisito chute == numeroSecreto
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 0 e 10');
-    if (chute == numeroSecreto){
-        alert(`Acertou, miseravi! na ${tentativas}° tentativa`);
+    chute = prompt(`${tentativas}° tentativa: Escolha um número entre 1 e 100`);
+    if (chute == numeroSecreto) {
+        // interrompe o processo
+        break; 
     } else {
-        if (chute > numeroSecreto){
+        if (chute > numeroSecreto) {
             alert(`Errou! Tente um número MENOR que ${chute}`);
         } else {
             alert(`Errou! Tente um número MAIOR que ${chute}`);
         }
-    tentativas++
+        tentativas++
     } 
 }
-    
+
+//operador ternário no JS
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+/*
+if (tentativas > 1) {
+    alert(`Acertou, miseravi! Após ${tentativas} tentativas.`);
+} else {
+    alert(`Acertou, miseravi! De primeira!`);
+}
+*/ 
